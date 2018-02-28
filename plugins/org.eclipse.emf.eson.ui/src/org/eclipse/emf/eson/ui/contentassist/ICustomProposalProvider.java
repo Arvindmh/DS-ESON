66aff1e643766a35a03092ab597258adb3fb4957
phanikumark <phanikumark@L638DGH2.asia.temenosgroup.com>
2018-02-28 12:00:12 +0530

    DS-14008:headers added

--- plugins/org.eclipse.emf.eson.ui/src/org/eclipse/emf/eson/ui/contentassist/ICustomProposalProvider.java
+++ plugins/org.eclipse.emf.eson.ui/src/org/eclipse/emf/eson/ui/contentassist/ICustomProposalProvider.java
@@ -1,3 +1,15 @@
+/*
+ * #%L
+ * org.eclipse.emf.eson.ui
+ * %%
+ * Copyright (C) 2009 - 2014 Sebastian Benz, Michael Vorburger and others
+ * %%
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * #L%
+ */
 package org.eclipse.emf.eson.ui.contentassist;
 
 import org.eclipse.emf.ecore.EAttribute;
